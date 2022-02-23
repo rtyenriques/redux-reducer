67a0510b071cb403555f73b59893ab07db81ff27
= <rtyenriques@gmail.com>
2022-02-23 02:19:34 -0800

    commit tree 6f9a1843e16450aa0be203700a5a077799483971

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return{friends: [...state.friends, {
+                name: action.friend.name, 
+                hometown: action.friend.hometown,
+                id: action.friend.id
+            } ]};
+        case "REMOVE_FRIEND":
+            const newFriends = state.friends.filter(friend => 
+                friend.id !== action.id
+            )
+            return{friends: newFriends}
+        default:
+            return state
+    }
 }
